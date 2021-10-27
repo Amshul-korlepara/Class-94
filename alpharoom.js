0f857b47a6d7d03a73ff84ad63439233e8ca6499
Amshul-korlepara <82725729+Amshul-korlepara@users.noreply.github.com>
2021-10-27 15:18:47 -0500

    Add files via upload

---
+++ alpharoom.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBQRUim3-ndf-57ZGL0GgXW2azw3jqUCbM",
+    authDomain: "chat-app-2-ec404.firebaseapp.com",
+    databaseURL: "https://chat-app-2-ec404-default-rtdb.firebaseio.com",
+    projectId: "chat-app-2-ec404",
+    storageBucket: "chat-app-2-ec404.appspot.com",
+    messagingSenderId: "858168261460",
+    appId: "1:858168261460:web:e48f01638c8ee538c426c1",
+    measurementId: "G-BGBZ8Z1PGZ"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
